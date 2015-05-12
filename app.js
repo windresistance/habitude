58227f20b2ce2fd9bc60f014cb3dd3a8c538cbb8
Nick Jones <jonesnickb@gmail.com>
2015-05-11 17:03:40 -0700

    changes prior to code school tutorial

--- app.js
+++ app.js
@@ -20,4 +20,4 @@ app.use('/api', require('./routes/api'));
 
 // Start server
 app.listen(3000);
-console.log('Express server running on port 3000');
+console.log('Express server running Habitude on port 3000!');
---
+++ controllers/tasks.js
@@ -0,0 +1,10 @@
+// var mongoose = require('mongoose');
+// var task = mongoose.model('task');
+
+// module.exports = {
+
+// 	index: function(req, res) {
+// 		res.render()
+// 	}
+
+// }
