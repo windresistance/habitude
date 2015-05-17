a485a41522c7a0b742afe3c1e15ccc0689c30a7e
Nick Jones <jonesnickb@gmail.com>
2015-05-17 00:08:50 -0700

    changed some styles

--- public/client.js
+++ public/client.js
@@ -8,9 +8,10 @@ $(function(){
 		var content, task;
 		for (var i in tasks) {
 			task = tasks[i];
+
 			content = '<a href="#" data-task-del="'+task._id+'">X</a>' + 
-					  '<a href="#" data-task-edit="'+task._id+'">edit</a>' + 
-					  '<a href="/tasks/'+task._id+'" class="name">'+task.name+'</a>';
+					  '<a href="/tasks/'+task._id+'" class="name">'+task.name+'</a>' + 
+					  '<a href="#" data-task-edit="'+task._id+'">edit</a>';
 			// list.push($('<li>', { html: content }));
 			list.push('<li id="' + task._id + '">' + content + '</li>');
 		}
@@ -56,6 +57,8 @@ $(function(){
 				  '<a href="#" data-task-save="'+task_id+'">save</a>' + 
 				  '<a href="#" data-task-cancel="'+task_id+'">cancel</a>';
 		target.parent().html(content);
+		
+		$('input[value="'+task_name+'"]').focus();  // move cursor to input
 	});
 
 	// update task
@@ -71,8 +74,8 @@ $(function(){
 			var task_id = target.data('task-save');
 
 			content = '<a href="#" data-task-del="'+task_id+'">X</a>' + 
-					  '<a href="#" data-task-edit="'+task_id+'">edit</a>' + 
-					  '<a href="/tasks/'+task_id+'" class="name">'+task_name+'</a>';
+					  '<a href="/tasks/'+task_id+'" class="name">'+task_name+'</a>' + 
+					  '<a href="#" data-task-edit="'+task_id+'">edit</a>';
 
 			target.parent().html(content);
 		});
@@ -85,8 +88,8 @@ $(function(){
 		var task_name = target.siblings('input').val();
 
 		content = '<a href="#" data-task-del="'+task_id+'">X</a>' + 
-				  '<a href="#" data-task-edit="'+task_id+'">edit</a>' + 
-				  '<a href="/tasks/'+task_id+'" class="name">'+task_name+'</a>';
+				  '<a href="/tasks/'+task_id+'" class="name">'+task_name+'</a>' + 
+				  '<a href="#" data-task-edit="'+task_id+'">edit</a>';
 		target.parent().html(content);
 	});
 });
